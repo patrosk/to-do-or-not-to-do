a4d07ffefef6f5f4b56ca7f6e574b74c0bf5686e
patrosk <naiade99@gmail.com>
2022-01-07 00:02:51 +0100

    finished nav bar styling

--- views/navigation.php
+++ views/navigation.php
@@ -7,48 +7,22 @@
                 <button class="dropdown-button">
                     <span class="hamburger">
                         <span class="hamburger-inner"></span>
-                        <!-- <div></div>
-                        <div></div>
-                        <div></div> -->
                     </span>
                 </button>
                 <ul class="dropdown-content">
-                    <li class="nav-item">
-                        <a class="nav-link" href="/index.php">Home</a>
-                    </li>
-
-                    <li class="nav-item">
-                        <a class="nav-link" href="/about.php">About</a>
-                    </li>
+                    <li><a href="/index.php">Home</a></li>
+                    <li><a href="/about.php">About</a></li>
 
                     <?php if (is_logged_in()) : ?>
-                        <li class="nav-item">
-                            <a class="nav-link" href="../profile.php">Profile</a>
-                        </li>
-                        <li class="nav-item">Tasks
-                            <ul>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="/overview.php">Overview</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="/lists.php">Your lists</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="/tasks.php">Your tasks</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/app/users/logout.php">Logout</a>
-                        </li>
+                        <li><a href="../profile.php">Profile</a></li>
+                        <li><a href="/overview.php">Overview</a></li>
+                        <li><a href="/lists.php">Your lists</a></li>
+                        <li><a href="/tasks.php">Your tasks</a></li>
+                        <li><a href="/app/users/logout.php">Logout</a></li>
 
                     <?php else : ?>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/login.php">Login</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/register.php">Register</a>
-                        </li>
+                        <li><a href="/login.php">Login</a></li>
+                        <li><a href="/register.php">Register</a></li>
                     <?php endif ?>
                 </ul>
             </div>
