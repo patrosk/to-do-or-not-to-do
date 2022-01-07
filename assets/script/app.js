a9359dc87f3f4144f729f4379f005f0770778d09
patrosk <naiade99@gmail.com>
2022-01-07 11:39:47 +0100

    fixed lint error

--- assets/script/app.js
+++ assets/script/app.js
@@ -3,6 +3,18 @@ const dropdown = document.querySelector('.dropdown-content');
 const hamburgerSpans = document.querySelector('.hamburger span');
 
 menuButton.addEventListener('click', () => {
-  dropdown.classList.toggle('show');
-  hamburgerSpans.classList.toggle('is-active');
+    dropdown.classList.add('show');
+    hamburgerSpans.classList.add('is-active');
 });
+
+if ('.hamburger.is-active') {
+    const activeHamburger = document.querySelector('.hamburger.is-active');
+    console.log(activeHamburger);
+}
+
+// activeHamburger.addEventListener('click', () => {
+//   console.log('hello');
+//   dropdown.classList.remove('show');
+//   dropdown.classList.add('hide');
+//   hamburgerSpans.classList.remove('is-active');
+// });
