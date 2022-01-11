0012fe9329bf664f9a31d4dd6aa4420f2cb7a2da
patrosk <naiade99@gmail.com>
2022-01-11 21:52:03 +0100

    media queries styling

--- lists.php
+++ lists.php
@@ -6,29 +6,35 @@ require __DIR__ . '/views/messages.php'; ?>
 
 <?= show_message() ?>
 
-<h2>Your Lists</h2>
+<div class="content">
 
-<ul>
-    <?php
-    $lists = get_lists($database);
-    foreach ($lists as $list) : ?>
-        <li class="list-titles">
-            <h2><?= $list['title'] ?></h2>
-            <button>
-                <a href="/single_list.php?id=<?= $list['id']; ?>">Edit list</a>
-            </button>
-            <button class="delete">
-                <a href="/app/lists/delete.php?id=<?= $list['id']; ?>" onclick="return confirm('Are you sure you want to delete this list and all tasks in it?');">Delete list</a>
-            </button>
-        </li>
-    <?php endforeach ?>
-</ul>
+    <div class="lists-list">
+        <h2>Your Lists</h2>
+        <ul>
+            <?php
+            $lists = get_lists($database);
+            foreach ($lists as $list) : ?>
+                <li class="list-titles">
+                    <h2><?= $list['title'] ?></h2>
+                    <button>
+                        <a href="/single_list.php?id=<?= $list['id']; ?>">Edit list</a>
+                    </button>
+                    <button class="delete">
+                        <a href="/app/lists/delete.php?id=<?= $list['id']; ?>" onclick="return confirm('Are you sure you want to delete this list and all tasks in it?');">Delete list</a>
+                    </button>
+                </li>
+            <?php endforeach ?>
+        </ul>
+    </div>
 
-<h3>Create New List</h3>
-<form action="/app/lists/create.php" method="post">
-    <label for="title">Name your list:</label>
-    <input name="title" id="title" type="text">
-    <button type="submit">Create List</button>
-</form>
+    <div class="create-new-list">
+        <h3>Create New List</h3>
+        <form action="/app/lists/create.php" method="post">
+            <label for="title">Name your list:</label>
+            <input name="title" id="title" type="text">
+            <button type="submit">Create List</button>
+        </form>
+    </div>
+</div>
 
 <?php require __DIR__ . '/views/footer.php';
