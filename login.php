7849582647d91dcd903ff30ca5f247808956da90
patrosk <naiade99@gmail.com>
2022-01-12 11:58:33 +0100

    desktop styling

--- login.php
+++ login.php
@@ -8,14 +8,16 @@ require __DIR__ . '/views/navigation.php'; ?>
 } ?>
 
 <h2>Login</h2>
-<form action="/app/users/login.php" method="post">
-    <label for="email">email address:</label>
-    <input type="email" name="email" id="email"><br>
-    <label for="password">password:</label>
-    <input type="password" name="password" id="password"><br>
-    <div class="submit-button">
-        <button type="submit">Login</button>
-    </div>
-</form>
+<div class="login">
+    <form action="/app/users/login.php" method="post">
+        <label for="email">email address:</label>
+        <input type="email" name="email" id="email"><br>
+        <label for="password">password:</label>
+        <input type="password" name="password" id="password"><br>
+        <div class="submit-button">
+            <button type="submit">Login</button>
+        </div>
+    </form>
+</div>
 
 <?php require __DIR__ . '/views/footer.php';
--- register.php
+++ register.php
@@ -8,16 +8,18 @@ require __DIR__ . '/views/navigation.php'; ?>
 } ?>
 
 <h2>Register</h2>
-<form action="/app/users/register.php" method="post">
-    <label for="username">user name:</label><br>
-    <input name="username" id="username" type="text" required><br>
-    <label for="email">email address:</label><br>
-    <input name="email" id="email" type="email" required><br>
-    <label for="password">select your password (at least 10 characters):</label><br>
-    <input name="password" id="password" type="password" minlength="10" required>
-    <div class="submit-button">
-        <button type="submit">Register</button>
-    </div>
-</form>
+<div class="register">
+    <form action="/app/users/register.php" method="post">
+        <label for="username">user name:</label><br>
+        <input name="username" id="username" type="text" required><br>
+        <label for="email">email address:</label><br>
+        <input name="email" id="email" type="email" required><br>
+        <label for="password">select your password (at least 10 characters):</label><br>
+        <input name="password" id="password" type="password" minlength="10" required>
+        <div class="submit-button">
+            <button type="submit">Register</button>
+        </div>
+    </form>
+</div>
 
 <?php require __DIR__ . '/views/footer.php';
